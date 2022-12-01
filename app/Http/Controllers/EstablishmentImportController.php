5b031dce72a0ab6799879b69266650513910eb73
Mark Maravillo <mjmaravillo@gmail.com>
2022-12-01 15:56:13 +0800

    Fixed establishment import functionality.

--- app/Http/Controllers/EstablishmentImportController.php
+++ app/Http/Controllers/EstablishmentImportController.php
@@ -16,15 +16,15 @@ class EstablishmentImportController extends Controller
 
     public function upload()
     {
-        $latest = Establishment::latest()->get()[0];
+        if (!Establishment::latest()->get()[0]) {
+            $attributes = request()->validate([
+                'establishments_list' => ['required']
+            ]);
 
-        $attributes = request()->validate([
-            'establishments_list' => ['required']
-        ]);
+            $imports = Excel::import(new EstablishmentsImport(), $attributes['establishments_list']);
+        }
 
-        $imports = Excel::import(new EstablishmentsImport(), $attributes['establishments_list']);
-
-        activity('Establishment Record Imported')->log('Establishment record(s) has been imported. ID of the latest Establishment before importing: ' . $latest->id);
+        activity('Establishment Record Imported')->log('Establishment record(s) has been imported. ID of the latest Establishment before importing: ');
 
         return redirect(route('establishments.index'))->with('success', 'You have successfully imported an Establishments List.');
     }
